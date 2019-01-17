3598d33120132911927236494ac767d8b1268bcb
Bryan Anthony Garcia <bryan@xam-consulting.com>
2019-01-17 21:35:39 +0800

    Add project files.

---
+++ ThisIsASampleRegression/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML.Data;
+using System;
+using System.IO;
+
+namespace ThisIsASampleRegression
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static TextLoader _textLoader;
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext(seed: 0);
+
+            _textLoader = mlContext.Data.CreateTextReader(new TextLoader.Arguments()
+            {
+                Separator = ",",
+                HasHeader = true,
+                Column = new[]
+                {
+                    new TextLoader.Column("VendorId", DataKind.Text, 0),
+                    new TextLoader.Column("RateCode", DataKind.Text, 1),
+                    new TextLoader.Column("PassengerCount", DataKind.R4, 2),
+                    new TextLoader.Column("TripTime", DataKind.R4, 3),
+                    new TextLoader.Column("TripDistance", DataKind.R4, 4),
+                    new TextLoader.Column("PaymentType", DataKind.Text, 5),
+                    new TextLoader.Column("FareAmount", DataKind.R4, 6)
+                }
+            });
+
+            Console.WriteLine("Create and train the model");
+            var model = Train(mlContext, _trainDataPath);
+
+            Console.WriteLine("Evaluate the model");
+            Evaluate(mlContext, model);
+
+            Console.WriteLine("Predict fare using the model");
+            Predict(mlContext, model);
+
+            Console.ReadLine();
+        }
+
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+            IDataView dataView = _textLoader.Read(dataPath);
+
+            var pipeline = mlContext.Transforms.CopyColumns("FareAmount", "Label")
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("VendorId"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("RateCode"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding("PaymentType"))
+                .Append(mlContext.Transforms.Concatenate("Features", "VendorId", "RateCode", "PassengerCount", "TripTime", "TripDistance", "PaymentType"))
+                .Append(mlContext.Regression.Trainers.FastTree());
+
+            var model = pipeline.Fit(dataView);
+
+            return model;
+        }
+
+        private static void Evaluate(MLContext mlContext, ITransformer model)
+        {
+            IDataView dataView = _textLoader.Read(_testDataPath);
+            var predictions = model.Transform(dataView);
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+
+            Console.WriteLine($"*       R2 Score:      {metrics.RSquared:0.##}");
+            Console.WriteLine($"*       RMS loss:      {metrics.Rms:#.##}");
+        }
+
+        private static void Predict(MLContext mlContext, ITransformer model)
+        {
+            var predictionFunction = model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(mlContext);
+            var taxiTripSample = new TaxiTrip()
+            {
+                VendorId = "VTS",
+                RateCode = "1",
+                PassengerCount = 1,
+                TripTime = 1620,
+                TripDistance = 10.67f,
+                PaymentType = "CRD",
+                FareAmount = 0
+            };
+
+            var prediction = predictionFunction.Predict(taxiTripSample);
+
+            Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 15.5");
+        }
+    }
+}
---
+++ ThisIsASampleRegression/TaxiTrip.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThisIsASampleRegression
+{
+    public class TaxiTrip
+    {
+        [Column("0")]
+        public string VendorId;
+
+        [Column("1")]
+        public string RateCode;
+
+        [Column("2")]
+        public float PassengerCount;
+
+        [Column("3")]
+        public float TripTime;
+
+        [Column("4")]
+        public float TripDistance;
+
+        [Column("5")]
+        public string PaymentType;
+
+        [Column("6")]
+        public float FareAmount;
+    }
+
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
